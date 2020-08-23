a97b548127c0b7fa5f4f72e51671a07d2a5d5746
JoaoLuizDeveloper <juninhoworld2012@gmail.com>
2020-08-23 11:45:14 -0300

    Details begin

--- UpLiftCurse/Areas/Customer/Controllers/HomeController.cs
+++ UpLiftCurse/Areas/Customer/Controllers/HomeController.cs
@@ -5,23 +5,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using UpLiftCurse.AccessData.Data.Repository.IRepository;
 using UpLiftCurse.Models;
+using UpLiftCurse.Models.ViewModels;
 
 namespace UpLiftCurse.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private HomeVM HomeVM;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-           
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM = new HomeVM()
+            {
+                CategoryList = _unitOfWork.Category.GetAll(),
+                ServiceList = _unitOfWork.Service.GetAll(includeProperties:"Frequency")
+            };
+
+            return View(HomeVM);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var serviceFromDb = _unitOfWork.Service.GetFirstOrDefault(includeProperties: "Category,Frequency", filter: c => c.Id == id);
+            return View(serviceFromDb);
         }
 
         public IActionResult Privacy()
